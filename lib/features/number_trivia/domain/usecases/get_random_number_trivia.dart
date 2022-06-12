d4e0289bd37ef5eb09e70be228d443ce7ebb14e4
Rob Murphy <rmurphyswimmer@gmail.com>
2022-06-12 21:13:06 +0200

    episode 3 complete

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,3 +1,16 @@
-class GetRandomNumberTrivia{
+import 'package:dartz/dartz.dart';
+import 'package:tdd_clean_architecture/core/error/failure.dart';
+import 'package:tdd_clean_architecture/core/use_cases/use_case.dart';
+import 'package:tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
+class GetRandomNumberTrivia extends UseCase {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(params) async {
+    return await repository.getRandomNumberTrivia();
+  }
 }
